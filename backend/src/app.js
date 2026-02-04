5906a92bca931b480cc5dcc38eaf4bc23655395e
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-02-04 22:22:44 +0500

    Fixed the Responsiveness for the mobile

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,22 +4,13 @@ const app = express()
 const PORT = 8000
 
 // Request Handler (Order of the Routes Matters)
+app.use( "/user",()=>{
 
-
-
-app.get("/user/:name/:age/:role", (req, res)=>{
-    console.log(req.params)
-    res.send("User Data us Avalible")
 })
-
-app.post("/user", (req, res)=>{
-    res.send("Send User Data ")
-})
-
-
 
 
 app.listen(PORT, ()=>{
     console.log(`Server Successfully Listening on Port ${PORT}`)
 })
 
+ 
